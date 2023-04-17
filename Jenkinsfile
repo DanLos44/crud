dd234092eb2b9477c9cb4783121dc0f5299c7032
daniel.losev <daniellosev95@gmail.com>
2023-04-17 11:19:16 +0300

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,37 @@
+
+pipeline {
+    agent {
+        label 'docker'
+    }
+    environment {
+    	DOCKERHUB_CREDENTIALS = credentials('daniellosev-dockerhub')
+    	}
+    
+    stages {
+       stage('Build') {
+          steps {
+             echo 'Building image'       	
+             sh 'docker build -t daniellosev/weather:mongoapp .'
+          }
+       }
+       stage('Test') {
+          steps {
+             echo 'Testing site'
+             dir('/home/ubuntu/workspace/Weatherhgfjhfhjhf/app'){    	
+	     	sh 'python3 -m unittest test_app.py'
+             }
+          }
+       }
+ }
+       post {
+         always {
+          sh 'sudo docker logout'          
+          }
+        success {
+                slackSend ( channel: '#successful-build', token: 'y3BoT2qJbqZXhyLfgWAMULI4', message: "Everything is good")
+        }
+        failure {
+                slackSend( channel: "#devops-alerts", token: 'y3BoT2qJbqZXhyLfgWAMULI4', message: "Test failed")
+        }
+          }
+ }
---
+++ app.py
@@ -0,0 +1,45 @@
+from bson import ObjectId
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+password = os.environ['MONGO_PASSWORD']
+client = MongoClient(f'mongodb+srv://daniellosev95:{password}@cluster0.9w7khno.mongodb.net/test')
+db = client['mydatabase']
+collection = db['mycollection']
+
+
+
+@app.route('/')
+def index():
+    data = list(collection.find())
+    return render_template('index.html', data=data)
+
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    record = {
+        'name': request.form.get('name'),
+        'age': request.form.get('age')
+    }
+    collection.insert_one(record)
+    return redirect('/')
+
+@app.route('/update/<id>', methods=['POST'])
+def update(id):
+    record = {
+        'name': request.form.get('name'),
+        'age': request.form.get('age')
+    }
+    collection.update_one({'_id': ObjectId(id)}, {'$set': record})
+    return redirect('/')
+
+@app.route('/delete/<id>')
+def delete(id):
+    collection.delete_one({'_id': ObjectId(id)})
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
